f8963ddccd5b74cf850988606d28e474bff1f66e
conao3 <conao3@gmail.com>
2019-03-19 02:26:16 +0900

    add header/:str.svg method

--- handler.ts
+++ handler.ts
@@ -6,6 +6,8 @@ import * as handler from 'serverless-express/handler';
 import * as svg2png from 'svg2png';
 
 
+//////////////////////////////////////////////////
+
 const app: express.Application = express();
 
 app.use(bodyParser.json({ strict: false }));
@@ -16,6 +18,24 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
     next()
 });
 
+const github_header_svg = (str:string, forground:string, background:string) => {
+    const result = `<svg xmlns="http://www.w3.org/2000/svg" width="1000" height="170">
+<style xmlns="http://www.w3.org/2000/svg" type="text/css">
+@import url('https://fonts.googleapis.com/css?family=Sarabun:100');
+text {font-size: 70px; font-family: 'Sarabun', sans-serif; font-weight: 100;}
+</style>
+<g>
+<rect x="0" y="0" width="100%" height="100%" fill="#${background}"></rect>
+</g>
+<g fill="#${forground}" font-family="Open Sans">
+<text x="90" y="120" font-size="50">${str}</text>
+</g>
+</svg>`;
+    return result;
+};
+
+//////////////////////////////////////////////////
+
 app.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
     res.json({apis : "/hello"});
 });
@@ -23,5 +43,18 @@ app.get('/', async (req: express.Request, res: express.Response): Promise<void> 
 app.get('/hello', async (req: express.Request, res: express.Response): Promise<void> => {
     res.json({message : "hello world"});
 });
+
+//////////////////////////////////////////////////
+
+app.get('/header/:str.svg', async (req: express.Request, res: express.Response): Promise<void> => {
+    const str = req.params.str || 'Unknown';
+    const forground = req.query.forground || 'FFF';
+    const background = req.query.background || '222';
+
+    res.type('svg');
+    res.send(github_header_svg(str, forground, background));
+});
+
+//////////////////////////////////////////////////
 
 exports.api = handler(app);
